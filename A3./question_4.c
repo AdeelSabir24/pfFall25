ea1b1111e3b467ed4fdeb461845689be12d3f3da
Adeel Sabar <nadegamingadeel69@gmail.com>
2025-11-26 18:39:02 +0500

    Update question_4.c

--- A3./question_4.c
+++ A3./question_4.c
@@ -1,82 +1,118 @@
 #include <stdio.h>
-#include <string.h>
 
-struct Book {
-  int id;
-  int popularity;
-  int lastAccess;
-};
+#define MAX 100
+
+typedef struct {
+    int id;
+    int popularity;
+    int lastAccess;
+    int valid;
+} Book;
+
+int findBook(Book shelf[], int capacity, int id);
+int findLRU(Book shelf[], int capacity);
+void addBook(Book shelf[], int capacity, int *timeCounter, int id, int popularity);
+int accessBook(Book shelf[], int capacity, int *timeCounter, int id);
 
 int main() {
-  int capacity, Q;
-  scanf("%d %d", &capacity, &Q);
+    int capacity;
+    printf("Enter shelf capacity: ");
+    scanf("%d", &capacity);
 
-  struct Book shelf[100];
-  int count = 0;
-  int timer = 0;
+    Book shelf[MAX] = {0};
+    int timeCounter = 1;
 
-  for (int i = 0; i < Q; i++) {
-    char command[10];
-    scanf("%s", command);
+    int choice;
+    while (1) {
+        printf("\n1. ADD book\n2. ACCESS book\n3. Exit\nEnter choice: ");
+        scanf("%d", &choice);
 
-    if (strcmp(command, "ADD") == 0) {
-      int id, pop;
-      scanf("%d %d", &id, &pop);
-
-      int found = -1;
-
-      for (int j = 0; j < count; j++) {
-        if (shelf[j].id == id) {
-          found = j;
-          break;
+        if (choice == 1) {
+            int id, pop;
+            printf("Enter Book ID and Popularity: ");
+            scanf("%d %d", &id, &pop);
+            addBook(shelf, capacity, &timeCounter, id, pop);
         }
-      }
+        else if (choice == 2) {
+            int id;
+            printf("Enter Book ID to access: ");
+            scanf("%d", &id);
+            int result = accessBook(shelf, capacity, &timeCounter, id);
+            if (result == -1)
+                printf("Book not found! Returned: -1\n");
+            else
+                printf("Popularity Score = %d\n", result);
+        }
+        else if (choice == 3) {
+            break;
+        }
+        else {
+            printf("Invalid choice!\n");
+        }
+    }
 
-      if (found != -1) {
-        shelf[found].popularity = pop;
-        shelf[found].lastAccess = ++timer;
-      } else {
-        if (count < capacity) {
-          shelf[count].id = id;
-          shelf[count].popularity = pop;
-          shelf[count].lastAccess = ++timer;
-          count++;
-        } else {
-          int lruIndex = 0;
-          for (int j = 1; j < count; j++) {
-            if (shelf[j].lastAccess < shelf[lruIndex].lastAccess) {
-              lruIndex = j;
+    return 0;
+}
+
+int findBook(Book shelf[], int capacity, int id) {
+    for (int i = 0; i < capacity; i++) {
+        if (shelf[i].valid == 1 && shelf[i].id == id)
+            return i;
+    }
+    return -1;
+}
+
+int findLRU(Book shelf[], int capacity) {
+    int lruIndex = -1;
+    int minAccess = 999999;
+
+    for (int i = 0; i < capacity; i++) {
+        if (shelf[i].valid == 1) {
+            if (shelf[i].lastAccess < minAccess) {
+                minAccess = shelf[i].lastAccess;
+                lruIndex = i;
             }
-          }
-
-          shelf[lruIndex].id = id;
-          shelf[lruIndex].popularity = pop;
-          shelf[lruIndex].lastAccess = ++timer;
         }
-      }
+    }
+    return lruIndex;
+}
+
+void addBook(Book shelf[], int capacity, int *timeCounter, int id, int popularity) {
+    int idx = findBook(shelf, capacity, id);
+
+    if (idx != -1) {
+        shelf[idx].popularity = popularity;
+        shelf[idx].lastAccess = (*timeCounter)++;
+        printf("Book updated & moved to most recently used.\n");
+        return;
     }
 
-    else if (strcmp(command, "ACCESS") == 0) {
-      int id;
-      scanf("%d", &id);
-
-      int found = -1;
-
-      for (int j = 0; j < count; j++) {
-        if (shelf[j].id == id) {
-          found = j;
-          break;
+    for (int i = 0; i < capacity; i++) {
+        if (shelf[i].valid == 0) {
+            shelf[i].id = id;
+            shelf[i].popularity = popularity;
+            shelf[i].lastAccess = (*timeCounter)++;
+            shelf[i].valid = 1;
+            printf("Book added to empty slot.\n");
+            return;
         }
-      }
-
-      if (found == -1) {
-        printf("-1\n");
-      } else {
-        shelf[found].lastAccess = ++timer;
-        printf("%d\n", shelf[found].popularity);
-      }
     }
-  }
 
-  return 0;
+    int lruIndex = findLRU(shelf, capacity);
+    printf("Shelf full! Removing LRU book with ID %d\n", shelf[lruIndex].id);
+
+    shelf[lruIndex].id = id;
+    shelf[lruIndex].popularity = popularity;
+    shelf[lruIndex].lastAccess = (*timeCounter)++;
+}
+
+int accessBook(Book shelf[], int capacity, int *timeCounter, int id) {
+    int idx = findBook(shelf, capacity, id);
+
+    if (idx == -1)
+        return -1;
+
+    shelf[idx].lastAccess = (*timeCounter)++;
+
+    return shelf[idx].popularity;
 }
